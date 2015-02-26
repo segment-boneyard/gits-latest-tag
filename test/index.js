9e31f17dbb71e84d3555e86cea43659afa7a836f
Travis Jeffery <tj@travisjeffery.com>
2015-02-26 16:04:06 -0500

    Stub github requests

--- test/index.js
+++ test/index.js
@@ -1,24 +1,28 @@
 var assert = require('assert');
-var GitHub = require('github');
-var netrc = require('netrc')();
 var semver = require('semver');
 
-describe('latestTag', function () {
-  var github = new GitHub({ version: '3.0.0' });
-  github.authenticate({
-    type: 'oauth',
-    token: netrc['api.github.com'].password
-  });
+/**
+ * Stubbed github.
+ */
 
+var github = {
+  repos: {
+    getTags: function(obj, cb){
+      cb(null, [{ name: 'v1.0.0' }, { name: 'non-semver' }]);
+    }
+  }
+};
+
+describe('latestTag', function () {
   var latestTag = require('..')(github);
 
   it('should get the latest tag', function *() {
-    var tag = yield latestTag('travisjeffery', 'timecop');
-    assert(tag.indexOf('v') == 0);
+    var tag = yield latestTag('travisjeffery', 'blah');
+    assert(tag.indexOf('v1.0.0') == 0);
   });
 
   it('should only allow semver tags when configured', function *() {
-    var tag = yield latestTag('segmentio', 'app');
+    var tag = yield latestTag('travisjeffery', 'blah');
     assert(semver.valid(tag));
   });
 });
