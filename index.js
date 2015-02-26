89529c5d7e79c34ced51b89b4610b06b83dab294
Travis Jeffery <tj@travisjeffery.com>
2015-02-26 16:04:21 -0500

    Use promises

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-var thunkify = require('thunkify');
 var semver = require('semver');
 var assert = require('assert');
 
@@ -14,24 +12,31 @@ module.exports = latestTag;
  *
  * @param {GitHub} github
  * @return {Function}
+ *   @param {String} user
+ *   @param {String} repo
+ *   @return {Promise}
  */
 
-function latestTag (github) {
+function latestTag(github) {
   assert(github, 'Must pass a github instance.');
 
-  return function *(user, repo) {
-    var repos = github.repos;
-    var get = thunkify(repos.getTags.bind(repos));
-    var all = yield get({ user: user, repo: repo });
+  return function(user, repo) {
+    return new Promise(function(resolve, reject){
+      github.repos.getTags({user: user, repo: repo}, function(err, tags){
+        if (err) return reject(err);
 
-    var valid = all
-      .filter(function (tag) {
-        return semver.valid(tag.name);
+        var tag = tags
+          .filter(function(tag){
+            return semver.valid(tag.name);
+          })
+          .sort(function(a, b){
+            return semver.compare(a.name, b.name);
+          })
+          .pop()
+          .name;
+
+        resolve(tag);
       })
-      .sort(function (a, b) {
-        return semver.compare(a.name, b.name);
-      });
-
-    return valid.pop().name;
+    });
   };
 };
