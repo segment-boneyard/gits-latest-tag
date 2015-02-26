b106c3a673572e7a4a627f2697dd8a16bb8a1bbd
Dominic Barnes <dominic@segment.io>
2015-02-26 20:08:31 +0000

    only retrieving valid semver tags, tests use netrc for token

--- test/index.js
+++ test/index.js
@@ -1,19 +1,25 @@
 var assert = require('assert');
 var co = require('co');
-var config = require('./config');
 var GitHub = require('github');
-var github = new GitHub({ version: '3.0.0' });
+var netrc = require('netrc')();
+var semver = require('semver');
 
 describe('latestTag', function () {
-  var latestTag;
-
-  before(function() {
-    github.authenticate({ type: 'oauth', token: config.token });
-    latestTag = require('..')(github);
+  var github = new GitHub({ version: '3.0.0' });
+  github.authenticate({
+    type: 'oauth',
+    token: netrc['api.github.com'].password
   });
 
-  it('should get the latest tag', co(function *() {
+  var latestTag = require('..')(github);
+
+  it('should get the latest tag', function *() {
     var tag = yield latestTag('travisjeffery', 'timecop');
     assert(tag.indexOf('v') == 0);
-  }));
+  });
+
+  it('should only allow semver tags when configured', function *() {
+    var tag = yield latestTag('segmentio', 'app');
+    assert(semver.valid(tag));
+  });
 });
